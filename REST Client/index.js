2304f39e3464603909c55d6825f7ef04c3f27609
Mach Bui <nicemach107@gmail.com>
2022-05-14 06:30:04 -0700

    Added aesthetics for database purposes

--- REST Client/index.js
+++ REST Client/index.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 app.set('port', 4020)
 
-// To implement the logic to process the HTTP request for static files 
-app.use(express.static(__dirname + '/app'))	// ./
+app.use(express.static(__dirname + '/app'))
 
 app.listen(app.get('port'), function(){
 	console.log('Express server started on http://localhost:' + app.get('port'));
